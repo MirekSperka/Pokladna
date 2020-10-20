dae7cde836260ba4f786cee42632037e33575edb
MirekSperka <72432816+MirekSperka@users.noreply.github.com>
2020-10-20 09:22:36 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokladna
+{
+ public partial class Form1 : Form
+ {
+  List<PokladniZaznam> pokladna;
+  IRepos repositar;
+
+  public Form1()
+  {
+   InitializeComponent();
+  }
+
+  private void Form1_Load(object sender, EventArgs e)
+  {
+   JsonRepos jsonRepos = new JsonRepos("data.json");
+   jsonRepos.VytvorTestData();
+   repositar = jsonRepos; 
+   
+   //repositar = new SqlRepos();
+   //repositar = new XmlRepos();
+   pokladna = repositar.NactiVse();
+   foreach (var p in pokladna)
+   {
+    lvData.Items.Add(p.DoLvItem());
+   }
+  }
+ }
+}
---
+++ PokladniZaznam.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokladna
+{
+ public class PokladniZaznam
+ {
+  public int IdPokladniZaznam { get; set; }
+  public int Cislo { get; set; }
+  public DateTime Datum { get; set; }
+  public string Popis { get; set; }
+  public double Castka { get; set; }
+  public double Zustatek { get; set; }
+  public string Poznamka { get; set; }
+
+  public PokladniZaznam()
+  {
+  }
+
+  public PokladniZaznam(int idPokladniZaznam, int cislo, DateTime datum, string popis, double castka, double zustatek, string poznamka)
+  {
+   IdPokladniZaznam = idPokladniZaznam;
+   Cislo = cislo;
+   Datum = datum;
+   Popis = popis;
+   Castka = castka;
+   Zustatek = zustatek;
+   Poznamka = poznamka;
+  }
+
+  public ListViewItem DoLvItem()
+  {
+   if (Castka > 0)
+   {
+    return new ListViewItem(new string[] { Datum.ToString("dd.MM.yyyy")
+                                         , Cislo.ToString()
+                                         , Popis
+                                         , Castka.ToString()
+                                         , ""
+                                         , Zustatek.ToString()
+                                         , Poznamka });
+   }
+   else
+   {
+    return new ListViewItem(new string[] { Datum.ToString("dd.MM.yyyy")
+                                         , Cislo.ToString()
+                                         , Popis
+                                         , ""
+                                         , Math.Abs(Castka).ToString()
+                                         , Zustatek.ToString()
+                                         , Poznamka });
+   }
+  }
+ }
+}
